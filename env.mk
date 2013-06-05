28d8fc01a5f203e4417d0949bcccce60c7338765
Charles Briere <charlesbriere.flatzo@gmail.com>
2013-06-04 22:19:01 -0400

    Updating tabs and removing unused vars

--- env.mk
+++ env.mk
@@ -1,25 +1,25 @@
 include fill_out.mk
 
-NDK_TOOLCHAIN 		:= ${NDK}/toolchains/arm-linux-androideabi-4.6/prebuilt/${BUILD_PLATFORM}/bin
+NDK_TOOLCHAIN		:= ${NDK}/toolchains/arm-linux-androideabi-4.6/prebuilt/${BUILD_PLATFORM}/bin
 PLATFORM_TOOLS		:= ${SDK}/platform-tools
-PATH				:= ${PLATFORM_TOOLS}:${PATH}
+PATH			:= ${PLATFORM_TOOLS}:${PATH}
 
 # Toolchain environment
 # ==========================================
 
 HOST			:= arm-linux-androideabi
-CROSS_COMPILE 	:= arm-linux-androideabi
+CROSS_COMPILE		:= arm-linux-androideabi
 SYSROOT			:= ${NDK}/platforms/${PLATFORM}/arch-arm
 
-CC				:= ${NDK_TOOLCHAIN}/${CROSS_COMPILE}-gcc
-CXX				:= ${NDK_TOOLCHAIN}/${CROSS_COMPILE}-g++
-LD				:= ${NDK_TOOLCHAIN}/${CROSS_COMPILE}-ld
-AR				:= ${NDK_TOOLCHAIN}/${CROSS_COMPILE}-ar
+CC			:= ${NDK_TOOLCHAIN}/${CROSS_COMPILE}-gcc
+CXX			:= ${NDK_TOOLCHAIN}/${CROSS_COMPILE}-g++
+LD			:= ${NDK_TOOLCHAIN}/${CROSS_COMPILE}-ld
+AR			:= ${NDK_TOOLCHAIN}/${CROSS_COMPILE}-ar
 RANLIB			:= ${NDK_TOOLCHAIN}/${CROSS_COMPILE}-ranlib
 STRIP			:= ${NDK_TOOLCHAIN}/${CROSS_COMPILE}-strip
 
-ac_cv_func_malloc_0_nonnull := yes
-ac_cv_func_realloc_0_nonnull := yes
+ac_cv_func_malloc_0_nonnull	:= yes
+ac_cv_func_realloc_0_nonnull	:= yes
 
 export ARCH
 export HOST
@@ -39,7 +39,7 @@ export ac_cv_func_realloc_0_nonnull
 # Compilation flags 
 # ==========================================
 
-CPPFLAGS 	:= --sysroot=${SYSROOT} -I${SYSROOT}/usr/include -I${SYSROOT}/include -I${INSTALL_PATH}/include -D_FORTIFY_SOURCE=0
+CPPFLAGS	:= --sysroot=${SYSROOT} -I${SYSROOT}/usr/include -I${SYSROOT}/include -I${INSTALL_PATH}/include -D_FORTIFY_SOURCE=0
 CXXFLAGS	:= --sysroot=${SYSROOT}
 CFLAGS		:= --sysroot=${SYSROOT}
 LDFLAGS		:= --sysroot=${SYSROOT} -L${SYSROOT}/usr/lib -L${SYSROOT}/lib -L${INSTALL_PATH}/lib
@@ -49,4 +49,4 @@ export CXXFLAGS
 export CFLAGS
 export LDFLAGS
 
-CONFIGURE_OPTIONS 	:= --host=${HOST} --target=${HOST} --prefix=${INSTALL_PATH}
+CONFIGURE_OPTIONS	:= --host=${HOST} --target=${HOST} --prefix=${INSTALL_PATH}
--- fill_out.mk
+++ fill_out.mk
@@ -1,7 +1,6 @@
 # Path to the root directories
-NDK             	:= /opt/android-ndk-r8e
-SDK             	:= /opt/android-sdk
-ANDROID_SOURCE  	:= /home/charles/android-source
+NDK			:= /opt/android-ndk-r8e
+SDK			:= /opt/android-sdk
 
 # Targeted Android platform version.
 #
@@ -11,7 +10,7 @@ ANDROID_SOURCE  	:= /home/charles/android-source
 # API versions can be found on : 
 #   http://developer.android.com/about/dashboards/index.html
 #
-PLATFORM        	:= android-14
+PLATFORM		:= android-14
 
 # Where to install temporary files on build machine
 INSTALL_PATH		:= /tmp/lttng-android
@@ -19,7 +18,7 @@ INSTALL_PATH		:= /tmp/lttng-android
 # Where lttng will be installed on the device
 # WARNING : Should not be the same path
 #           as the one containing .../var/run
-TARGET_INSTALL_PATH := /data/lttng-install
+TARGET_INSTALL_PATH	:= /data/lttng-install
 
 # Build platform 
 # Select the one that fits best
