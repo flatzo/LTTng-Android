60e73c093ac9ba46e3692cf52c068f0a7dbcc731
Charles Briere <charlesbriere.flatzo@gmail.com>
2013-06-07 00:00:39 -0400

    Clearing env.mk to make it more readable

--- env.mk
+++ env.mk
@@ -17,47 +17,27 @@ PATH			:= ${PLATFORM_TOOLS}:${PATH}
 # Toolchain environment
 # ==========================================
 
-ARCH			:= arm
-HOST			:= arm-linux-androideabi
-CROSS_COMPILE		:= ${NDK_TOOLCHAIN}/arm-linux-androideabi-
-SYSROOT			:= ${NDK}/platforms/${PLATFORM}/arch-arm
+export ARCH			:= arm
+export HOST			:= arm-linux-androideabi
+export CROSS_COMPILE		:= ${NDK_TOOLCHAIN}/arm-linux-androideabi-
+export SYSROOT			:= ${NDK}/platforms/${PLATFORM}/arch-arm
 
-CC			:= ${CROSS_COMPILE}gcc
-CXX			:= ${CROSS_COMPILE}g++
-LD			:= ${CROSS_COMPILE}ld
-AR			:= ${CROSS_COMPILE}ar
-RANLIB			:= ${CROSS_COMPILE}ranlib
-STRIP			:= ${CROSS_COMPILE}strip
+export CC			:= ${CROSS_COMPILE}gcc
+export CXX			:= ${CROSS_COMPILE}g++
+export LD			:= ${CROSS_COMPILE}ld
+export AR			:= ${CROSS_COMPILE}ar
+export RANLIB			:= ${CROSS_COMPILE}ranlib
+export STRIP			:= ${CROSS_COMPILE}strip
 
-ac_cv_func_malloc_0_nonnull	:= yes
-ac_cv_func_realloc_0_nonnull	:= yes
-
-export ARCH
-export HOST
-export CROSS_COMPILE
-export SYSROOT
-
-export CC
-export CXX
-export LD
-export AR
-export RANLIB
-export STRIP
-
-export ac_cv_func_malloc_0_nonnull
-export ac_cv_func_realloc_0_nonnull
+export ac_cv_func_malloc_0_nonnull	:= yes
+export ac_cv_func_realloc_0_nonnull	:= yes
 
 # Compilation flags 
 # ==========================================
 
-CPPFLAGS	:= --sysroot=${SYSROOT} -I${SYSROOT}/usr/include -I${SYSROOT}/include -I${INSTALL_PATH}/include -D_FORTIFY_SOURCE=0
-CXXFLAGS	:= --sysroot=${SYSROOT}
-CFLAGS		:= --sysroot=${SYSROOT}
-LDFLAGS		:= --sysroot=${SYSROOT} -L${SYSROOT}/usr/lib -L${SYSROOT}/lib -L${INSTALL_PATH}/lib
-
-export CPPFLAGS
-export CXXFLAGS
-export CFLAGS
-export LDFLAGS
+export CPPFLAGS	:= --sysroot=${SYSROOT} -I${SYSROOT}/usr/include -I${SYSROOT}/include -I${INSTALL_PATH}/include -D_FORTIFY_SOURCE=0
+export CXXFLAGS	:= --sysroot=${SYSROOT}
+export CFLAGS		:= --sysroot=${SYSROOT}
+export LDFLAGS		:= --sysroot=${SYSROOT} -L${SYSROOT}/usr/lib -L${SYSROOT}/lib -L${INSTALL_PATH}/lib
 
 CONFIGURE_OPTIONS	:= --host=${HOST} --target=${HOST} --prefix=${INSTALL_PATH}
